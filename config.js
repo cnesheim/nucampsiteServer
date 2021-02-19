c62ebc9a639ccfdc651458715319b3d6a3dcd36c
Christian Nesheim <cnesheim@gmail.com>
2021-02-19 11:06:29 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1556033321257927',
+        clientSecret: 'b10e1f20d7e40d915edd9e22e600bbd8'
+    }
 }
